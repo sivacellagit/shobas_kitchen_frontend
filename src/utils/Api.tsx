c49e6b5f91d95f9cb0f655f4b1cba8c425eefe52
sivacellagit <sivakumar.saravanavel@gmail.com>
2025-08-23 11:55:53 +0100

    Update API baseURL for production

--- src/utils/Api.tsx
+++ src/utils/Api.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 
 
 const api = axios.create({
- baseURL: "http://localhost:8000/api",
+// baseURL: "http://localhost:8000/api",
+ baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/",
  headers: {
    "Content-Type": "application/json",
  },
