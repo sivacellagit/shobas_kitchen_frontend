f8ea8a6f594744eee62656541a1ebaa3272a602a
sivacellagit <sivakumar.saravanavel@gmail.com>
2025-08-26 16:55:38 +0100

    Updated API baseURL with in env for production

--- src/utils/Api.tsx
+++ src/utils/Api.tsx
@@ -4,8 +4,8 @@ import axios from "axios";
 
 const api = axios.create({
 // baseURL: "http://localhost:8000/api",
-// baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/",
-baseURL: "https://api.shobaskitchen.in/api/",
+baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/",
+//baseURL: "https://api.shobaskitchen.in/api/",
  headers: {
    "Content-Type": "application/json",
  },
